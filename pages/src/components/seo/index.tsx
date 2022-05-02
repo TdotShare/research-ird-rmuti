459c78485118c13a24b3144b0867013de2db733b
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2022-05-02 21:01:23 +0700

    update title name
    update title name

--- pages/src/components/seo/index.tsx
+++ pages/src/components/seo/index.tsx
@@ -7,7 +7,7 @@ const SEO: NextPage = () => {
         <>
             <Head>
                 <title>{titleConfig.NameWeb}</title>
-                <meta name="description" content="MISIRD RMUTI" />
+                <meta name="description" content={`${titleConfig.NameWeb}`} />
                 <link rel="icon" href="./irdrmuti.gif" />
                 <link rel="apple-touch-icon" href="./irdrmuti.gif" />
 
@@ -17,25 +17,25 @@ const SEO: NextPage = () => {
 
 
                 <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
-         
+
                 <link rel="stylesheet" href="template/fontawesome-free/css/all.min.css" />
 
-                <link rel="stylesheet" href="template/styles/bootstrap.min.css" />
-                <link rel="stylesheet" href="template/vendor/css/animate.css" />
                 <link rel="stylesheet" href="template/vendor/css/slick.css" />
+                <link rel="stylesheet" href="template/vendor/css/animate.css" />
                 <link rel="stylesheet" href="template/vendor/css/venobox.min.css" />
+                <link rel="stylesheet" href="template/styles/bootstrap.min.css" />
                 <link rel="stylesheet" href="template/styles/main.css" />
 
-                <script async src="template/scripts/jquery-3.5.1.min.js"/>
-                <script async src="template/scripts/popper.min.js"  />
+                <script async src="template/scripts/jquery-3.5.1.min.js" />
+                <script async src="template/scripts/popper.min.js" />
                 <script async src="template/scripts/bootstrap.min.js" />
 
-
+                <script async src="template/vendor/js/wow.min.js" />
                 <script async src="template/vendor/js/slick.min.js" />
                 <script async src="template/vendor/js/venobox.min.js" />
                 <script async src="template/vendor/js/jquery.counterup.min.js" />
                 <script async src="template/vendor/js/waypoints.min.js" />
-                <script async src="template/vendor/js/wow.min.js" />
+
                 <script async src="template/scripts/main.js" />
             </Head>
         </>
